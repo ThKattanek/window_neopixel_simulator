56fbbddb0cf2f825f18fa3eeec1dae8710ac5240
Thorsten Kattanek <thorsten.kattanek@gmx.de>
2021-11-14 20:30:22 +0100

    final cometrain

--- cometrain.h
+++ cometrain.h
@@ -2,6 +2,22 @@
 #define COMETRAIN_H
 
 #include <inttypes.h>
+#include <iostream>
+
+#define COMET_COUNT 20
+
+using namespace std;
+
+struct comet{
+	int posx;
+	float posy;
+	uint32_t color;
+	float life;
+	float life_speed;
+	int len;
+	float speed;
+	bool dead;
+};
 
 class Cometrain
 {
@@ -13,8 +29,14 @@ public:
 	void Render();
 
 private:
+	void DrawPixel(int x, int y, uint32_t color);
+	void BornComet(comet &c);
+	bool DrawComet(comet &c);
+
 	int xw, yw, led_count;
 	uint32_t *buffer;
+
+	comet c1[COMET_COUNT];
 };
 
 #endif // COMETRAIN_H
